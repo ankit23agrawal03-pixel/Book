fd57dad520df09a074a46f6a980fe807d6409ad4
ankit23agrawal03-pixel <ankit23agrawal03@gmail.com>
2026-05-10 00:16:12 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../arz-e-haal.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
